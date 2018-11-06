a038a0cf8de338d28716b214109e1fa9478da76d
Danilo Caporale <danilo.caporale@gmail.com>
2018-11-06 23:29:29 +0100

    IMUComponent
    Working on IMU Component. Does not work yet.

--- Source/LidarRays/Sensors/Private/IMUComponent.cpp
+++ Source/LidarRays/Sensors/Private/IMUComponent.cpp
@@ -1,1 +1,40 @@
 #include "IMUComponent.h"
+
+
+UIMUComponent::UIMUComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+UIMUComponent::~UIMUComponent()
+{
+}
+
+void UIMUComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	InitTime = GetWorld()->GetTimeSeconds();
+
+	Owner = GetOwner();
+	World = GetWorld();
+
+	if (!Owner)
+	{
+		UE_LOG(LogTemp, Error, TEXT("IMU component has no owner"));
+	}
+
+	if (!World)
+	{
+		UE_LOG(LogTemp, Error, TEXT("IMU component has no world"));
+	}
+}
+
+void UIMUComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	
+}
--- Source/LidarRays/Sensors/Public/IMUComponent.h
+++ Source/LidarRays/Sensors/Public/IMUComponent.h
@@ -4,10 +4,41 @@
 #include "Components/ActorComponent.h"
 #include "IMUComponent.generated.h"
 
+class AActor;
+class UWorld;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FIMUAvailableDelegate, UIMUMessage*, IMUSample);
+
+
 UCLASS(ClassGroup = (Sensors), meta = (BlueprintSpawnableComponent))
 class LIDARRAYS_API UIMUComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-	
+	AActor* Owner = nullptr;
+	UWorld* World = nullptr;
+
+public:
+
+	UIMUComponent();
+	~UIMUComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorCompnentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(BlueprintAssignable, Category = IMU)
+	FIMUAvailableDelegate OnIMUAvailable;
+
+	UPROPERTY(EditAnywhere, Category = IMU)
+	bool DebugIMU = false;
+
+	FVector GetIMUOrientation() const;
+	FVector GetIMUAngularVelocity() const;
+	FVector GetIMULinearAcceleration() const;
+
 };
